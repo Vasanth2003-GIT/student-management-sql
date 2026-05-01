5607f60062925284279248ddbe8017e8d24c518f
Vasanth2003-GIT <vasanthb112003@gmail.com>
2026-05-01 13:43:37 +0530

    Add files via upload
    This project is a Student Management System implemented using SQL.
    It includes tables for students, courses, and marks, and demonstrates
    database design, data insertion, and querying using JOIN, GROUP BY,
    and aggregate functions.
    
    This project helped me understand relational database concepts and SQL query execution

---
+++ Student_Management.sql
@@ -0,0 +1,26 @@
+-- Create Tables
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50)
+);
+
+CREATE TABLE Courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50)
+);
+
+CREATE TABLE Marks (
+    mark_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    marks INT,
+    FOREIGN KEY (student_id) REFERENCES Students(student_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);
+
+-- Insert Data
+INSERT INTO Students VALUES
+(1, 'Vasanth', 'ECE'),
+(2, 'Arun', 'CSE'),
+(3, 'vicky', 'IT');
