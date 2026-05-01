f4a2c8e2782ddc923e5541fc9b4143ffc0f0f7b5
Vasanth2003-GIT <vasanthb112003@gmail.com>
2026-05-01 13:55:50 +0530

    Update Student_Management.sql

--- Student_Management.sql
+++ Student_Management.sql
@@ -24,3 +24,28 @@ INSERT INTO Students VALUES
 (1, 'Vasanth', 'ECE'),
 (2, 'Arun', 'CSE'),
 (3, 'vicky', 'IT');
+
+TO GET OUTPUT:
+
+--JOIN: Get student name, course, and marks
+
+SELECT s.name, c.course_name, m.marks
+FROM Students s
+JOIN Marks m ON s.student_id = m.student_id
+JOIN Courses c ON m.course_id = c.course_id;
+
+
+ -- GROUP BY: Average marks per student
+ 
+SELECT student_id, AVG(marks) AS avg_marks
+FROM Marks
+GROUP BY student_id;
+
+
+ -- Top student based on total marks
+ 
+SELECT s.name, SUM(m.marks) AS total_marks
+FROM Students s
+JOIN Marks m ON s.student_id = m.student_id
+GROUP BY s.name
+ORDER BY total_marks DESC;
